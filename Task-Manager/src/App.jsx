c316c840c12456482f9231ed96f84c7e110907e2
Kelvin Ndoma <kelvinndomamutua@gmail.com>
2025-07-18 20:59:00 +0300

    added components and auth

--- Task-Manager/src/App.jsx
+++ Task-Manager/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import React, { useContext } from 'react'
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom"
 import Login from './pages/Auth/Login'
 import SignUp from './pages/Auth/SignUp'
 import PrivateRoute from './routes/PrivateRoute'
@@ -10,9 +10,11 @@ import ManageUsers from './pages/Admin/ManageUsers'
 import UserDashboard from './pages/User/UserDashboard'
 import MyTasks from './pages/User/MyTasks'
 import ViewTaskDetails from './pages/User/ViewTaskDetails'
+import UserProvider, { UserContext } from './context/userContext'
 
 const App = () => {
   return (
+    <UserProvider>
     <div>
       <Router>
         <Routes>
@@ -32,12 +34,26 @@ const App = () => {
             <Route path='/user/dashboard' element={<UserDashboard />} />
             <Route path='/user/tasks' element={<MyTasks />} />
             <Route path='/user/task-details/:id' element={<ViewTaskDetails />} />
-
           </Route>
+          {/* Default Route */}
+          <Route path='/' element={<Root />} />
         </Routes>
       </Router>
     </div>
+    </UserProvider>
   )
 }
 
 export default App
+
+const Root = () => {
+    const { user, loading } = useContext(UserContext);
+
+    if(loading) return <Outlet />
+
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+
+    return user.role === "admin" ? <Navigate to="/admin/dashboard" /> : <Navigate to="/user/dashboard" />;
+};
